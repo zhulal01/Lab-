1b9c1fa6fc5c8e1bc95d4313cc4d1f7e17e7a73a
zhulal01 <149515141+zhulal01@users.noreply.github.com>
2023-11-14 17:15:21 +0200

    Lab1

---
+++ z3.py
@@ -0,0 +1,13 @@
+"""
+Задача №3.
+Напишіть програму для друку літери Б висотою 5 рядків за допомогою введеного користувачем символу.
+
+
+Автор: Жулід А А
+"""
+symbol = input("Введіть символ: ")
+print(f'{symbol}{symbol}{symbol}{symbol}{symbol}')
+print(f'{symbol}')
+print(f'{symbol}{symbol}{symbol}{symbol}')
+print(f'{symbol}   {symbol}')
+print(f'{symbol}{symbol}{symbol}{symbol}')
